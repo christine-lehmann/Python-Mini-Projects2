92eb07272fed728259bc921759b2d8798b02a610
Christine Cion <christinemaecion@gmail.com>
2022-03-05 09:44:15 +0800

    new commit

--- getweatherinfo.py
+++ getweatherinfo.py
@@ -1,10 +1,12 @@
+# Create an account to get api key -> https://home.openweathermap.org/api_keys
+
 from urllib import request
 
 
 import requests
 from pprint import pprint
 
-API_Key = 'ad2e4a30dbda7091fcd33c21589606df'
+API_Key = 'ad2e4a30dbda7091fcd33c21589606df' # default key
 
 city = input("Enter a city: ")
 
